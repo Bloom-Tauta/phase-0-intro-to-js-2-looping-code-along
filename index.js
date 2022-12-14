4c322456184c6fcfaf84ede92c5ee73e422485b4
Bloom <bloomtauta@gmail.com>
2022-12-14 16:42:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ['Guadalupe','Ollie','Aki'];
+function writeCards(names) {
+    const array=[]
+  for(let i = 0; i < names.length; i++) {
+    array[i]=(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return array;
+}
+writeCards(names);
+
+
+function countDown() {
+    let i = 0;
+    while (i <= 10) { 
+        console.log(i++);
+    }
+}
+countDown();
